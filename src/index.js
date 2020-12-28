56045759969ee33c4603cb157367f7f4a8322f69
J3SUNG <wptjd6141@naver.com>
2020-12-27 18:58:58 -0800

    useState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
+  const [item, setItem] = useState(1);
   return (
     <div className="App">
       <h1>Hello React Hooks</h1>
-      <h2>Nice to meet you</h2>
+      <h2>item is {item}</h2>
     </div>
   );
 }
