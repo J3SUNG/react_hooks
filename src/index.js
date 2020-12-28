ec71fcdd9e8286df55b06d91add333b761209dff
J3SUNG <wptjd6141@naver.com>
2020-12-27 19:01:51 -0800

    added button useState

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-function App() {
+const App = () => {
   const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
       <h1>Hello React Hooks</h1>
       <h2>item is {item}</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>cerement</button>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
