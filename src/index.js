3191cebf71b4eb1c399d777cd62afbf5f3301904
J3SUNG <wptjd6141@naver.com>
2021-01-07 12:31:58 -0800

    study useTabs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import useInput from "./useInput";
+import useTabs from "./useTabs";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "This is the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "This is the content of the Section 2",
+  },
+];
 
 const App = () => {
   const maxLen = (value) => value.length <= 10;
   const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
       <h1>Hello</h1>
       <div>
         <div>useInput</div>
         <input placeholder="Name" {...name} />
+      </div>
+      <div>
+        <div>useTabs</div>
+        <div>
+          {content.map((section, index) => (
+            <button key={index} onClick={() => changeItem(index)}>
+              {section.tab}
+            </button>
+          ))}
+        </div>
+        <div>{currentItem.content}</div>
       </div>
     </div>
   );
--- src/useTabs.js
+++ src/useTabs.js
@@ -1,16 +1,4 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2",
-  },
-];
+import { useState } from "react";
 
 const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
@@ -23,21 +11,4 @@ const useTabs = (initialTab, allTabs) => {
   };
 };
 
-const App = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
-  return (
-    <div>
-      {content.map((section, index) => (
-        <button key={index} onClick={() => changeItem(index)}>
-          {section.tab}
-        </button>
-      ))}
-      <div>{currentItem.content}</div>
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-//
+export default useTabs;
