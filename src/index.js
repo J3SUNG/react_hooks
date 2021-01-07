c1daeeea475f019d2eef6f0e80af3d91f0b6e1d3
J3SUNG <wptjd6141@naver.com>
2021-01-07 11:28:08 -0800

    study useInput

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import useAxios from "./useAxios";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
 
 const App = () => {
-  const { loading, data, error, refetch } = useAxios({
-    url:
-      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
-  });
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
-    <div className="App" style={{ height: "1000vh" }}>
-      <h1>{data && data.status}</h1>
-      <h2>{loading && "Loading"}</h2>
-      <button onClick={refetch}>Refetch</button>
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
---
+++ src/useAxios_index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url:
+      "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
+  });
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
